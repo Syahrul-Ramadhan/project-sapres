d0f1c6048e5dd99e1df15de195c4019158c701b2
Syahrul Ramadhan <syahrulramadhansr00@gmail.com>
2025-06-11 11:41:34 +0700

    memodifikasi hal beasiswa

--- pages/beasiswa.php
+++ pages/beasiswa.php
@@ -17,6 +17,9 @@
     />
   </head>
   <body>
+    <?php
+    include "php/koneksi.php";
+    ?>
     <!-- NAVBAR START -->
     <nav class="navbar">
       <div class="nav-responsive">
@@ -587,7 +590,7 @@
           <h3>Daftar Beasiswa</h3>
           <div class="beasiswa-list">
             <a
-              href="detailBeasiswa.html"
+              href="detailBeasiswa.php"
               class="beasiswa-card-md btn-detail-beasiswa"
               data-category="sweden"
             >
@@ -626,187 +629,13 @@
                 <div class="dates">
                   <p class="start-date">Mulai: 17 Mar 2025</p>
                   <p class="deadline">Deadline: 02 Jun 2025</p>
-                </div>
-              </div>
-            </a>
-            <a
-              href="detailBeasiswa.html"
-              class="beasiswa-card-md btn-detail-beasiswa"
-              data-category="NTU"
-            >
-              <div class="card-content">
-                <div class="head-card">
-                  <div class="degrees">
-                    <span class="degree">S1</span>
-                    <span class="degree">S2</span>
-                  </div>
-                  <div class="bookmark">
-                    <div class="bookmark-btn">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                        color="#333332"
-                        fill="none"
-                      >
-                        <path
-                          d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
-                          stroke="currentColor"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-                <div class="body-card">
-                  <h2 class="title">NTU Singapore Global Connect Fellowship</h2>
-                  <p class="location">Singapura</p>
-                </div>
-                <div class="dates">
-                  <p class="start-date">Mulai: 21 Jan 2025</p>
-                  <p class="deadline">Deadline: 11 Feb 2025</p>
-                </div>
-              </div>
-            </a>
-            <a
-              href="detailBeasiswa.html"
-              class="beasiswa-card-md btn-detail-beasiswa"
-              data-category="gyeongsang"
-            >
-              <div class="card-content">
-                <div class="head-card">
-                  <div class="degrees">
-                    <span class="degree">S2</span>
-                    <span class="degree">S3</span>
-                  </div>
-                  <div class="bookmark">
-                    <div class="bookmark-btn">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                        color="#333332"
-                        fill="none"
-                      >
-                        <path
-                          d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
-                          stroke="currentColor"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-                <div class="body-card">
-                  <h2 class="title">
-                    Gyeongsang National University Scholarship
-                  </h2>
-                  <p class="location">Korea Selatan</p>
-                </div>
-                <div class="dates">
-                  <p class="start-date">Mulai: 01 Mar 2025</p>
-                  <p class="deadline">Deadline: 28 Mar 2025</p>
-                </div>
-              </div>
-            </a>
-            <a
-              href="detailBeasiswa.html"
-              class="beasiswa-card-md btn-detail-beasiswa"
-              data-category="fulbright"
-            >
-              <div class="card-content">
-                <div class="head-card">
-                  <div class="degrees">
-                    <span class="degree">S1</span>
-                    <span class="degree">S2</span>
-                  </div>
-                  <div class="bookmark">
-                    <div class="bookmark-btn">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                        color="#333332"
-                        fill="none"
-                      >
-                        <path
-                          d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
-                          stroke="currentColor"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-                <div class="body-card">
-                  <h2 class="title">
-                    Fulbright Foreign Language Teaching Assistant (FLTA)
-                  </h2>
-                  <p class="location">Amerika Serikat</p>
-                </div>
-                <div class="dates">
-                  <p class="start-date">Mulai: 10 Mar 2025</p>
-                  <p class="deadline">Deadline: 15 Apr 2025</p>
-                </div>
-              </div>
-            </a>
-            <a
-              href="detailBeasiswa.html"
-              class="beasiswa-card-md btn-detail-beasiswa"
-              data-category="matsumae"
-            >
-              <div class="card-content">
-                <div class="head-card">
-                  <div class="degrees">
-                    <span class="degree">S3</span>
-                  </div>
-                  <div class="bookmark">
-                    <div class="bookmark-btn">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                        color="#333332"
-                        fill="none"
-                      >
-                        <path
-                          d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
-                          stroke="currentColor"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-                <div class="body-card">
-                  <h2 class="title">
-                    Matsumae International Foundation Research S3 Fellowship
-                    2026
-                  </h2>
-                  <p class="location">Jepang</p>
-                </div>
-                <div class="dates">
-                  <p class="start-date">Mulai: 01 Mar 2025</p>
-                  <p class="deadline">Deadline: 30 Jun 2025</p>
                 </div>
               </div>
             </a>
           </div>
           <div class="beasiswa-list">
             <a
-              href="detailBeasiswa.html"
+              href="detailBeasiswa.php"
               class="beasiswa-card btn-detail-beasiswa"
               data-category="sweden"
             >
@@ -825,180 +654,6 @@
                     Swedish Collegium Advanced Study Fellowship Programme 2026
                   </h2>
                   <p class="location">Swedia</p>
-                </div>
-                <div class="bookmark">
-                  <div class="bookmark-btn">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      width="24"
-                      height="24"
-                      color="#333332"
-                      fill="none"
-                    >
-                      <path
-                        d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-            </a>
-            <a
-              href="detailBeasiswa.html"
-              class="beasiswa-card btn-detail-beasiswa"
-              data-category="NTU"
-            >
-              <div class="card-info">
-                <div class="degrees">
-                  <span class="degree">S1</span>
-                  <span class="degree">S2</span>
-                </div>
-                <div class="dates">
-                  <p class="start-date">Mulai: 21 Jan 2025</p>
-                  <p class="deadline">Deadline: 11 Feb 2025</p>
-                </div>
-              </div>
-              <div class="card-content">
-                <div class="head-card">
-                  <h2 class="title">NTU Singapore Global Connect Fellowship</h2>
-                  <p class="location">Singapura</p>
-                </div>
-                <div class="bookmark">
-                  <div class="bookmark-btn">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      width="24"
-                      height="24"
-                      color="#333332"
-                      fill="none"
-                    >
-                      <path
-                        d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-            </a>
-            <a
-              href="detailBeasiswa.html"
-              class="beasiswa-card btn-detail-beasiswa"
-              data-category="gyeongsang"
-            >
-              <div class="card-info">
-                <div class="degrees">
-                  <span class="degree">S2</span>
-                  <span class="degree">S3</span>
-                </div>
-                <div class="dates">
-                  <p class="start-date">Mulai: 01 Mar 2025</p>
-                  <p class="deadline">Deadline: 28 Mar 2025</p>
-                </div>
-              </div>
-              <div class="card-content">
-                <div class="head-card">
-                  <h2 class="title">
-                    Gyeongsang National University Scholarship
-                  </h2>
-                  <p class="location">Korea Selatan</p>
-                </div>
-                <div class="bookmark">
-                  <div class="bookmark-btn">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      width="24"
-                      height="24"
-                      color="#333332"
-                      fill="none"
-                    >
-                      <path
-                        d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-            </a>
-            <a
-              href="detailBeasiswa.html"
-              class="beasiswa-card btn-detail-beasiswa"
-              data-category="fulbright"
-            >
-              <div class="card-info">
-                <div class="degrees">
-                  <span class="degree">S1</span>
-                  <span class="degree">S2</span>
-                </div>
-                <div class="dates">
-                  <p class="start-date">Mulai: 10 Mar 2025</p>
-                  <p class="deadline">Deadline: 15 Apr 2025</p>
-                </div>
-              </div>
-              <div class="card-content">
-                <div class="head-card">
-                  <h2 class="title">
-                    Fulbright Foreign Language Teaching Assistant (FLTA)
-                  </h2>
-                  <p class="location">Amerika Serikat</p>
-                </div>
-                <div class="bookmark">
-                  <div class="bookmark-btn">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      width="24"
-                      height="24"
-                      color="#333332"
-                      fill="none"
-                    >
-                      <path
-                        d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-            </a>
-            <a
-              href="detailBeasiswa.html"
-              class="beasiswa-card btn-detail-beasiswa"
-              data-category="matsumae"
-            >
-              <div class="card-info">
-                <div class="degrees">
-                  <span class="degree">S3</span>
-                </div>
-                <div class="dates">
-                  <p class="start-date">Mulai: 01 Mar 2025</p>
-                  <p class="deadline">Deadline: 30 Jun 2025</p>
-                </div>
-              </div>
-              <div class="card-content">
-                <div class="head-card">
-                  <h2 class="title">
-                    Matsumae International Foundation Research S3 Fellowship
-                    2026
-                  </h2>
-                  <p class="location">Jepang</p>
                 </div>
                 <div class="bookmark">
                   <div class="bookmark-btn">
