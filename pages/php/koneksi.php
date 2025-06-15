66e883c941d9ca35993ebb609afaac010c6b7091
Syahrul Ramadhan <syahrulramadhansr00@gmail.com>
2025-06-15 15:44:43 +0700

    menambahkan fitur forum

--- pages/php/koneksi.php
+++ pages/php/koneksi.php
@@ -5,7 +5,7 @@
 
     $koneksi = mysqli_connect($server,$username,$password);
 
-    $database = "sapres";
+    $database = "db_sapres";
     mysqli_select_db($koneksi,$database) or die ("Database tidak ditemukan");
 
 
